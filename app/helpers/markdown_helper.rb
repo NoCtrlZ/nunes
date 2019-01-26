28662d37bfbe0501ef64b8d7f3eb840c99935754
Hemlocked <phantomofrotten@gmail.com>
2019-01-26 22:14:37 +0900

    add-markdown-helper

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,29 @@
+module MarkdownHelper
+    def markdown(text)
+      options = {
+        filter_html:         true,
+        hard_wrap:           true,
+        space_after_headers: true,
+        with_toc_data:       true,
+        fenced_code_blocks:  true
+      }
+
+      extensions = {
+        autolink:                     true,
+        no_intra_emphasis:            true,
+        fenced_code_blocks:           true,
+        tables:                       true,
+        footnotes:                    true,
+        strikethrough:                true,
+        lax_html_blocks:              true,
+        quote:                        true,
+        disable_indented_code_blocks: true,
+        no_intra_emphasis:            true
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+      markdown.render(text).html_safe
+    end
+
+  end
