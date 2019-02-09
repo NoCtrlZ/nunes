a0b88d646f438e8a2bdd95566379d72222939462
Hemlocked <phantomofrotten@gmail.com>
2019-02-09 17:15:36 +0900

    get-latest-5-article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find params[:id]
+        @articles = Article.order(created_at: :desc).limit(5).offset(0)
     end
 
 end
