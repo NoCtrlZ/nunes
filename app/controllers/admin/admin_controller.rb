5ae3746002f699f9453bd4e40ece9125e2046634
Hemlocked <phantomofrotten@gmail.com>
2019-02-11 01:16:49 +0900

    fix

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,5 @@
 class Admin::AdminController < ApplicationController
-	http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+	http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD'] if Rails.env == "production"
     layout "admin"
 
     def index
