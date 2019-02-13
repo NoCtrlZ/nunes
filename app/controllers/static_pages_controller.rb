38e23497c16ef94b0a4736962b4c616a6a596103
Hemlocked <phantomofrotten@gmail.com>
2019-02-13 19:10:57 +0900

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 	def index
-		@articles = Article.page(params[:page]).per(4)
+		@articles = Article.page(params[:page]).per(5).order(created_at: :desc)
 		@contact = Contact.new
 		@news = News.all
 	end
