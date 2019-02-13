26d88e03fa8db90cfb1be07b9779fb7a494455d7
Hemlocked <phantomofrotten@gmail.com>
2019-02-13 19:03:15 +0900

    add-layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 	def index
-		@articles = Article.page(params[:page]).per(10)
+		@articles = Article.page(params[:page]).per(4)
 		@contact = Contact.new
 		@news = News.all
 	end
