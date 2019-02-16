e650ef304361475c5293d07d91ca5bf427e91372
Hemlocked <phantomofrotten@gmail.com>
2019-02-17 01:44:26 +0900

    fix-admin-view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
 	def index
 		@displayarticles = Article.where(is_display: true)
-		@articles = @displayarticles.page(params[:page]).per(10).order(created_at: :desc)
+		@articles = @displayarticles.page(params[:page]).per(7).order(created_at: :desc)
 		@post = @displayarticles.order(created_at: :desc).limit(5).offset(0)
 		@contact = Contact.new
 		@news = News.all
